de2f6a00b2441d4d27952f3db325a146b4c53c13
blueneutron <vivs0766@gmail.com>
2021-06-22 19:56:36 +0530

    Revert "Demo for different type of services in android"
    This reverts commit 57c59760

---
+++ app/src/main/java/com/example/services/MessageService.kt
@@ -0,0 +1,37 @@
+package com.example.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import android.os.Message
+import android.os.Messenger
+import android.widget.Toast
+
+const val MSG_SAY_HELLO = 1
+
+class MessageService : Service() {
+
+    private lateinit var messenger: Messenger
+
+    internal class IncomingHandler(
+        context: Context,
+        private val applicationContext: Context = context.applicationContext
+    ) : Handler() {
+        override fun handleMessage(msg: Message) {
+
+            when (msg.what) {
+                MSG_SAY_HELLO ->
+                    Toast.makeText(applicationContext, "hello!", Toast.LENGTH_SHORT).show()
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        Toast.makeText(applicationContext, "binding", Toast.LENGTH_SHORT).show()
+        messenger = Messenger(IncomingHandler(this))
+        return messenger.binder
+    }
+}
